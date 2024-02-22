81bc0fb417d3932d69f4b391c2f976f6ff4fc83e
YanaSus <susljakovajana@yandex.ru>
2024-02-23 01:08:28 +0300

    Added animated background

--- src/components/form/MyForm.jsx
+++ src/components/form/MyForm.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
 import cl from './MyForm.module.css'
+import gifImg from '../../assets/images/cat.gif'
 
 const MyForm = () => {
     return (
         <div>
-            <div className={cl.form__box}>
-            <form action="">
-                <input type="text" placeholder='текст' />
-                <button>Отправить</button>
-            </form>
+            <div className={cl.container}>
+                <div className={cl.animation}>
+                    <div className={cl.glowing}>
+                        <span style={{ '--i': 1}}></span>
+                        <span style={{ '--i': 2}}></span>
+                        <span style={{ '--i': 3}}></span>
+                    </div>
+                    <div className={cl.glowing}>
+                        <span style={{ '--i': 1}}></span>
+                        <span style={{ '--i': 2}}></span>
+                        <span style={{ '--i': 3}}></span>
+                    </div>
+                    <div className={cl.glowing}>
+                        <span style={{ '--i': 1}}></span>
+                        <span style={{ '--i': 2}}></span>
+                        <span style={{ '--i': 3}}></span>
+                    </div>
+                    <div className={cl.glowing}>
+                        <span style={{ '--i': 1}}></span>
+                        <span style={{ '--i': 2}}></span>
+                        <span style={{ '--i': 3}}></span>
+                    </div>
+                </div>
+                <div className={cl.question__box}>
+                    <h1>Пойдём гулять?</h1>
+                    <img className={cl.gif} src={gifImg} alt="" />
+                    <div className={cl.question__btns}>
+                        <button>Да</button>
+                        <button>Нет</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
